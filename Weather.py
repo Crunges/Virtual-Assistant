c4ab8fbdbc0f8b2be8de141962a1ccc6692414c1
Crunges <104575597+Crunges@users.noreply.github.com>
2022-08-08 00:31:18 +0800

    Updated Weather.module
    Deleted unusing code

--- Weather.py
+++ Weather.py
@@ -1,18 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-#
-# url = 'https://pogoda.vtomske.ru/'  # Сам сайт прогноза погоды
-# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}  # User-Agent
-# full_page = requests.get(url, headers=headers)  # Получаем HTML разметку сайта
-# soup = BeautifulSoup(full_page.content, 'html.parser')  # Варим суп из 7 разметок
-#
-#
-# wd = soup.find("div", class_="left")  # 1 способ
-# wd_details = soup.find("div", class_="details")
-# print(wd.text)
-# print(wd_details.text)
-
-
 import requests
 import json
 city_name = input("Enter city:")
@@ -30,7 +15,6 @@ for data in json_data:
 api_url_base = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
 response = requests.get(api_url_base, params={'units': 'metric', 'lang': 'ru'})
 json_data = json.loads(response.text)
-print(json_data)
 print("conditions:", json_data['weather'][0]['description'])
 print("temp:", json_data['main']['temp'])
 print("temp_min:", json_data['main']['temp_min'])
